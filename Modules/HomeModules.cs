24b55c239058e7d65b7de8d60b9211f9f411ce65
sea413 <seanmp42@gmail.com>
2016-02-12 21:21:11 -0800

    Complete the files and allow for a readme submission.

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -12,57 +12,57 @@ namespace ContactBook
         Get["/"] = _ => {
             return View["index.cshtml"];
         };
-        Get["/categories"]=_=> {
+        Get["/officers"]=_=> {
           var allContacts = Contact.GetAll();
-         return View["categories.cshtml", allContacts];
+         return View["officers.cshtml", allContacts];
        };
 
-       Get["/categories/new"] = _ => {
-             return View["category_form.cshtml"];
+       Get["/officers/new"] = _ => {
+             return View["Officer_Rank_submit.cshtml"];
            };
-       Post["/categories"] = _ => {
+       Post["/officers"] = _ => {
            var newContact = new Contact(Request.Form["contact-name"]);
            var allContacts = Contact.GetAll();
-           return View["categories.cshtml", allContacts];
+           return View["officers.cshtml", allContacts];
          };
-        Get["/categories/{id}"] = parameters => {
+        Get["/officers/{id}"] = parameters => {
           Dictionary<string, object> model = new Dictionary<string, object>();
           var selectedContact = Contact.Find(parameters.id);
           var contactinfo = selectedContact.GetContactinfo();
           model.Add("contact", selectedContact);
           model.Add("cinfo", contactinfo);
-          return View["category.cshtml", model];
+          return View["officer_rank_info.cshtml", model];
         };
-        Get["/clear_categories"] =_=> {
+        Get["/clear_officers"] =_=> {
           Contact.ClearAll();
           return View ["index.cshtml"];
         };
-        Get["/categories/{id}/task/{taskId}"] = parameters => {
+        Get["/officers/{id}/personal_info/{personal_infoId}"] = parameters => {
           Dictionary<string, object> model = new Dictionary<string, object>();
           Contact selectedContact = Contact.Find(parameters.id);
-          cinfo returncinfo = cinfo.Find(parameters.taskId);
+          cinfo returncinfo = cinfo.Find(parameters.personal_infoId);
           model.Add("Contact", selectedContact);
           model.Add("cinfo", returncinfo);
-          return  View["task.cshtml",model];
+          return  View["personal_info.cshtml",model];
         };
-        Get["/categories/{id}/tasks/new"] = parameters => {
+        Get["/officers/{id}/officers_info_specfic/new"] = parameters => {
         Dictionary<string, object> model = new Dictionary<string, object>();
         Contact selectedContact = Contact.Find(parameters.id);
         List<cinfo> allcinfo = selectedContact.GetContactinfo();
         model.Add("contact", selectedContact);
         model.Add("cinfor", allcinfo);
-        return View["category_tasks.cshtml", model];
+        return View["officer_info.cshtml", model];
       };
-        Post["/categories/{id}/task/{taskId}"] = parameters => {
+        Post["/officers/{id}/personal_info/{personal_infoId}"] = parameters => {
           Dictionary<string, object> model = new Dictionary<string, object>();
           var selectedContact = Contact.Find(parameters.id);
           var contactCinfo = selectedContact.GetContactinfo();
-          cinfo cinfos = contactCinfo[parameters.taskId-1];
+          cinfo cinfos = contactCinfo[parameters.personal_infoId-1];
           model.Add("contact", selectedContact);
           model.Add("cinfo", contactCinfo);
-          return View["category.cshtml", model];
+          return View["officer_rank_info.cshtml", model];
         };
-        Post["/tasks"] = _ => {
+        Post["/officers_info_specfic"] = _ => {
           Dictionary<string, object> model = new Dictionary<string, object>();
           Contact selectedContact = Contact.Find(Request.Form["contact-id"]);
           List<cinfo> contactinfo = selectedContact.GetContactinfo();
@@ -73,7 +73,7 @@ namespace ContactBook
           contactinfo.Add(newcinfo);
           model.Add("cinfo", contactinfo);
           model.Add("contact", selectedContact);
-          return View["category.cshtml", model];
+          return View["officer_rank_info.cshtml", model];
         };
       }
     }
